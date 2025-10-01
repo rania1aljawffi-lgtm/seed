2bd8d3794acd2234fc6e524c75566bf57d3d537d
rania1aljawffi-lgtm <rania1aljawffi@gmail.com>
2025-10-01 23:46:37 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+// footer.php
+?>
+</main>
+<footer class="site-footer">
+  <div class="container">
+    <p>حقوق الملكية &copy; <?php echo date('Y'); ?> شركة بذور البطاطس</p>
+  </div>
+</footer>
+</body>
+</html>
